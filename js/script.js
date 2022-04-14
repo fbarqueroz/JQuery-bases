4f6519c9cbcb4c1f50efcac6448ce1a72e620e78
Fabian Barquero <fabianbarquero9@gmail.com>
2022-04-14 01:29:14 -0600

    Agrego contenido

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
-/* Basic syntax
+/* 
+Basic syntax
 
-// $ --> Access to JQuery library
-
-$(selector) // Inside the parentheses, write the selector
-
-$(selector).action(); // Define the action on the selector
-
+  - $ --> Access to JQuery library
+  * $(selector) // Inside the parentheses, write the selector
+  * $(selector).action(); // Define the action on the selector
 */
 
-// .ready() --> Check if the document is ready (without issues)
+/*
+  * .ready() --> Check if the document is ready (without issues)
+  - It's good practice to code JQuery inside the function --> document.ready()
+*/
 
 // Full version
 $(document).ready(() => {
-  console.log("Hello world!");
+  console.log("Hello world! (Full)");
 }); 
 
 // Short version
 $(() => {
-  console.log("Hello world!");
+  console.log("Hello world! (Short)");
 }); 
+
+/* 
+  * .hide() --> hide an element or label 
+  * "" --> refers to a label
+*/
+$(() => {
+  $("p").hide();
+});
